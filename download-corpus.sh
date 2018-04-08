d838df4cfadcdf1de3b2c21e271f17a9f17278aa
Viliam Rockai <viliam.rockai@gmail.com>
2018-04-08 09:00:22 +0200

    Fix the download data script

--- download-corpus.sh
+++ download-corpus.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 # From http://opus.nlpl.eu/OpenSubtitles2018.php
 # Contains actual subtitles
 wget -N http://opus.nlpl.eu/download.php?f=OpenSubtitles2018/en.tar.gz -P ./corpus/
+
+# Untar corpus if not already there
+if [ ! -d "./corpus/OpenSubtitles2018" ]; then
+  tar --extract --verbose --keep-old-files --file ./corpus/*OpenSubtitles2018*.tar.gz --directory ./corpus/
+fi
 
 # From https://www.opensubtitles.org/en/downloads#exports
 #      http://dl.opensubtitles.org/addons/export/
